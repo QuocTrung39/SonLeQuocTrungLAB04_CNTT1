ec388ade19af822357fea5a959ef2a986bbe05a5
QuocTrung39 <126407157+QuocTrung39@users.noreply.github.com>
2023-03-08 13:13:24 +0700

    Add files via upload

---
+++ test4.java
@@ -0,0 +1,10 @@
+package lab4;
+
+import lab4.DanhSachSinhVien;
+public class test4 {
+    public static void main (String[] arg)
+    {
+    DanhSachSinhVien dssv = new DanhSachSinhVien();
+    dssv.menu();
+     }
+}
